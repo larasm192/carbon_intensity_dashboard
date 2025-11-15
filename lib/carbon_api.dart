d05037804cef3ed2dd2627a4830b668461f0180c
larasm192 <lm323@ic.ac.uk>
2025-11-15 15:53:07 +0000

    colour changes based on carbon value

--- lib/carbon_api.dart
+++ lib/carbon_api.dart
@@ -2,13 +2,13 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
-class Carbon {
+class CurrentCarbon {
   final String from;
   final String to;
   final int intensity;
   final String index;
 
-  Carbon({
+  CurrentCarbon({
     required this.from,
     required this.to,
     required this.intensity,
@@ -16,13 +16,7 @@ class Carbon {
   });
 }
 
-class CarbonToday {
-  final List data;
-
-  CarbonToday({required this.data});
-}
-
-Future<Carbon> fetchCurrentIntensity() async {
+Future<CurrentCarbon> fetchCurrentIntensity() async {
   final response = await http.get(
     Uri.parse('https://api.carbonintensity.org.uk/intensity'),
     headers: {'Accept': 'application/json'},
@@ -35,12 +29,56 @@ Future<Carbon> fetchCurrentIntensity() async {
     final intensityData = data['intensity'];
     final value = intensityData['actual'];
 
-    return Carbon(
+    return CurrentCarbon(
       from: data['from'],
       to: data['to'],
       intensity: value,
       index: intensityData['index'],
     );
+  } else {
+    throw Exception('Failed to load current intensity');
+  }
+}
+
+class CarbonToday {
+  final String to;
+  final int actualIntensity;
+  final int forecastIntensity;
+
+  CarbonToday({
+    required this.to,
+    required this.actualIntensity,
+    required this.forecastIntensity,
+  });
+}
+
+Future<List<CarbonToday>> fetchTodayIntensities() async {
+  final response = await http.get(
+    Uri.parse('https://api.carbonintensity.org.uk/intensity/date'),
+    headers: {'Accept': 'application/json'},
+  );
+
+  if (response.statusCode == 200) {
+    final decoded = jsonDecode(response.body);
+    final List data = decoded['data'];
+
+    List<CarbonToday> intensityList = [];
+    for (var item in data) {
+      final to = item['to'] ?? '';
+      final intensity = item['intensity'] ?? {};
+      final actual = intensity['actual'] ?? 0;
+      final forecast = intensity['forecast'] ?? 0;
+
+      intensityList.add(
+        CarbonToday(
+          to: to,
+          actualIntensity: actual,
+          forecastIntensity: forecast,
+        ),
+      );
+    }
+
+    return intensityList;
   } else {
     throw Exception('Failed to load current intensity');
   }
--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'carbon_api.dart';
+import 'package:fl_chart/fl_chart.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
 
   @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  Color indexColour = Colors.grey;
+  String lastUpdated = "--:--";
+
+  @override
+  void initState() {
+    super.initState();
+    loadColour();
+  }
+
+  void loadColour() async {
+    final carbon = await fetchCurrentIntensity();
+
+    Color colour;
+    switch (carbon.index) {
+      case "low":
+        colour = Colors.green;
+        break;
+      case "moderate":
+        colour = Colors.orange;
+        break;
+      case "high":
+        colour = Colors.red;
+        break;
+      default:
+        colour = Colors.grey;
+    }
+
+    setState(() {
+      indexColour = colour;
+      // Time extracted from API "from" field - only HH:MM
+      lastUpdated = carbon.from.substring(11, 16);
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Last updated: 19:30',
-          style: TextStyle(fontSize: 15),
+        backgroundColor: indexColour,
+        title: Text(
+          'Last updated: $lastUpdated',
+          style: const TextStyle(fontSize: 15),
         ),
       ),
+
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -22,9 +62,9 @@ class Dashboard extends StatelessWidget {
             Container(
               height: 300,
               width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Color.fromRGBO(27, 154, 139, 100),
-                borderRadius: BorderRadius.only(
+              decoration: BoxDecoration(
+                color: indexColour,
+                borderRadius: const BorderRadius.only(
                   bottomLeft: Radius.circular(150),
                   bottomRight: Radius.circular(150),
                 ),
@@ -40,8 +80,8 @@ class Dashboard extends StatelessWidget {
 
             // Intensity Graph
             Container(
-              padding: const EdgeInsets.all(12),
-              child: const SizedBox(height: 280),
+              padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+              child: const SizedBox(height: 280, child: IntensityGraph()),
             ),
           ],
         ),
@@ -60,7 +100,7 @@ class CurrentIntensity extends StatefulWidget {
 class _CurrentIntensityState extends State<CurrentIntensity> {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Carbon>(
+    return FutureBuilder<CurrentCarbon>(
       future: fetchCurrentIntensity(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
@@ -80,3 +120,74 @@ class _CurrentIntensityState extends State<CurrentIntensity> {
     );
   }
 }
+
+class IntensityGraph extends StatefulWidget {
+  const IntensityGraph({super.key});
+
+  @override
+  State<IntensityGraph> createState() => _IntensityGraphState();
+}
+
+class _IntensityGraphState extends State<IntensityGraph> {
+  @override
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(fontWeight: FontWeight.bold, fontSize: 16);
+    String text = switch (value.toInt()) {
+      2 => 'MAR',
+      5 => 'JUN',
+      8 => 'SEP',
+      _ => '',
+    };
+    return SideTitleWidget(
+      meta: meta,
+      child: Text(text, style: style),
+    );
+  }
+
+  Widget leftTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(fontWeight: FontWeight.bold, fontSize: 15);
+    String text = switch (value.toInt()) {
+      1 => '10K',
+      3 => '30k',
+      5 => '50k',
+      _ => '',
+    };
+
+    return Text(text, style: style, textAlign: TextAlign.left);
+  }
+
+  Widget build(BuildContext context) => LineChart(
+    LineChartData(
+      minX: 0,
+      maxX: 10,
+      titlesData: FlTitlesData(
+        show: true,
+        rightTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        topTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 30,
+            interval: 1,
+            getTitlesWidget: bottomTitleWidgets,
+          ),
+        ),
+        leftTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            interval: 1,
+            getTitlesWidget: leftTitleWidgets,
+            reservedSize: 42,
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+// Update value every 30 mins + time updated
+// Update colour according to index √
+// Convert CarbonToday to FLSpots somehow
+// Find a way to make x-axis in time format
